020d9c8be32959de9bf3ee979dc66b5e46e9ad11
Shane Mindreau <smindre1@binghamton.edu>
2023-10-30 13:25:34 -0400

    Edit to express.js for Heroku Deployment

--- express.js
+++ express.js
@@ -4,7 +4,7 @@ const express = require("express");
 // const { clog } = require('./middleware/clog');
 const routes = require("./routes");
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
